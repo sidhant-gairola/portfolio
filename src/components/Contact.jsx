7c879190f4f6fdfd136a155a6ccefecf2372d522
Sidhant Gairola <ssid7074@gmail.com>
2025-03-13 01:25:58 +0530

    Initial commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,24 @@
+import { CONTACT } from "../constants"
+import { motion } from 'framer-motion';
+
+const Contact = () => {
+    return (
+        <div className="border-b border-neutral-900 pb-20">
+            <motion.h2
+                whileInView={{ opacity: 1, y: 0 }}
+                initial={{ opacity: 0, y: -100 }}
+                transition={{ duration: 0.5 }}
+                className="my-10 text-center text-4xl">Get In Touch</motion.h2>
+            <motion.div
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: -100 }}
+                transition={{ duration: 1 }}
+                className="text-center tracking-tighter">
+                <p className="my-4"> {CONTACT.address}</p>
+                <a href="mailto:ssid7074@gmail.com" className="border-b">{CONTACT.email}</a>
+            </motion.div>
+        </div>
+    )
+}
+
+export default Contact
---
+++ src/constants/index.js
@@ -0,0 +1,47 @@
+import project1 from "../assets/projects/project-1.png";
+import project2 from "../assets/projects/project-2.png";
+import project3 from "../assets/projects/project-3.jpg";
+
+export const HERO_CONTENT = `I am a dedicated full-stack developer with expertise in Java programming and front-end technologies like HTML, CSS, JavaScript, React, and Tailwind CSS, along with hands-on experience in back-end development using Node.js, Express.js, and database management with MySQL and MongoDB. With a strong foundation in Artificial Intelligence and Machine Learning, I am passionate about designing and developing innovative, user-centric solutions that integrate advanced technologies to enhance user experience, optimize performance, and drive meaningful digital transformations.`;
+
+export const ABOUT_TEXT = `I am a full-stack developer skilled in Java, React, Tailwind CSS, Node.js, Express.js, MySQL, and MongoDB. Currently pursuing my MCA in Artificial Intelligence & Machine Learning, I enjoy building user-friendly and scalable applications. I love learning new things, solving problems, and working in teams. I am passionate about designing and developing innovative, user-centric solutions that integrate advanced technologies to enhance user experience, optimize performance, and drive meaningful digital transformations.
+Outside of coding, I enjoy playing volleyball, cricket, and badminton. I am also a guitarist and find music relaxing. Collaboration excites me because it helps me grow and improve as a developer.`;
+
+export const EXPERIENCES = [
+  {
+    year: "Feb 2025 - Present",
+    role: "Full Stack Developer",
+    company: "Pheme Software Pvt. Ltd.",
+    description: `Led a team in developing and maintaining a web application using React.js, Node.js, Express.js, and Tailwind CSS. Implemented RESTful APIs and integrated them with backend tools and technologies. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["React.js", "Node.js",  "Express.js","Tailwind CSS"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Personalized Treatment Planning System",
+    image: project1,
+    description:
+      "A web application that integrates user inputs and medical data through a RESTful API to provide tailored recommendations.",
+    technologies: ["React", "Tailwind CSS", "Vite", "Node.js", "Express"],
+},
+{
+    title: "Nritya Dance Academy Website",
+    image: project2,
+    description:
+      "A responsive website to enhance user engagement and streamline online services for a dance academy.",
+    technologies: ["HTML", "CSS", "JavaScript", "Node.js", "Express.js", "MongoDB"],
+},
+{
+    title: "Multimodal Emotion Recognition System",
+    image: project3,
+    description:
+      "An AI-based system that detects real-time emotions using multimodal (text and image) fusion for high accuracy.",
+    technologies: ["Python", "VGG16", "BERT", "PyTorch", "TensorFlow"],
+},
+];
+
+export const CONTACT = {
+  address: "Haridwar, Uttarakhand, India. ",
+  email: "ssid7074@gmail.com",
+};
